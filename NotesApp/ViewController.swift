b9a8094daf57c5b59ecfc497b2e1aa244c666c1d
Vladimir Izmaylov <holybuddha@yandex.ru>
2022-02-06 22:34:27 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Vladimir Izmaylov on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

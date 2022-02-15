34190a9cc6784694abf0a0a6e7f8ecf34e89f1a9
Vladimir Izmaylov <holybuddha@yandex.ru>
2022-02-16 02:51:05 +0300

    Refactoring StorageManager, Added sorting

--- NotesApp/NotesListViewController.swift
+++ NotesApp/NotesListViewController.swift
@@ -10,13 +10,12 @@ import CoreData
 
 class NotesListViewController: UITableViewController {
     
-    //Вынеси всю работу с coreData в StorageManager
-    
     private let context = StorageManager.shared.persistentContainer.viewContext
     private var notesList: [Note] = []
-    
-  
 
+    @IBAction func sort(_ sender: UIBarButtonItem) {
+            filterList(notesList)
+    }
     @IBAction func addNote(_ sender: UIBarButtonItem) {
             let alert = UIAlertController(title: "Add new note", message: "Please enter text", preferredStyle: .alert)
             let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
@@ -27,7 +26,7 @@ class NotesListViewController: UITableViewController {
             alert.addAction(saveAction)
             alert.addAction(cancelAction)
             alert.addTextField { textField in
-                textField.placeholder = "New Task"
+                textField.placeholder = "New Note"
             }
             present(alert, animated: true)
     }
@@ -43,10 +42,8 @@ class NotesListViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { notesList.count }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let note = notesList[indexPath.row]
@@ -55,91 +52,77 @@ class NotesListViewController: UITableViewController {
         cell.contentConfiguration = content
         return cell
     }
-}
-
-extension NotesListViewController {
     
-    private func save(_ noteName: String) {
-    
-        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Note", in: context) else { return }
-        guard let note = NSManagedObject(entity: entityDescription, insertInto: context) as? Note else { return }
-        note.text = noteName
-        notesList.append(note)
-        
-        let cellIndex = IndexPath(row: notesList.count - 1, section: 0)
-        tableView.insertRows(at: [cellIndex], with: .automatic)
-        
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch let error {
-                print(error)
-            }
-        }
-    }
-    
-    private func delete(_ note: Note) {
-        context.delete(note)
-        StorageManager.shared.saveContext()
-    }
-    
-    private func edit(note: Note, with noteName: String) {
-        note.text = noteName
-        StorageManager.shared.saveContext()
-        reloadData()
-    }
-    
-    private func fetchData() {
-        let fetchRequest = Note.fetchRequest()
-        do {
-            notesList = try context.fetch(fetchRequest)
-        } catch let error {
-            print("Failed to fetch data", error)
-        }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let note = notesList[indexPath.row]
+        showEditAlert(note)
     }
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
         let note = notesList[indexPath.row]
         let editAction = UIContextualAction(style: .normal, title: "Edit") { (contextualAction, view, boolValue) in
             self.showEditAlert(note)
             boolValue(true)
         }
-        
         editAction.backgroundColor = .systemBlue
-        
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {  (contextualAction, view, boolValue) in
-            
             self.notesList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            
-            self.delete(note)
+            StorageManager.shared.delete(note)
         }
-            
         let swipeActions = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
         
         return swipeActions
     }
+}
+
+extension NotesListViewController {
     
+    private func save(_ noteName: String) {
+        StorageManager.shared.save(noteName) { note in
+            self.notesList.append(note)
+            let cellIndex = IndexPath(row: notesList.count - 1, section: 0)
+            tableView.insertRows(at: [cellIndex], with: .automatic)
+        }
+    }
+
+    private func fetchData() {
+        StorageManager.shared.fetchData { result in
+            switch result {
+            case .success(let notes):
+                self.notesList = notes
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: - AlertController
     
     private func showEditAlert(_ noteForCell: Note) {
     let alert = UIAlertController(title: "Edit this note", message: "Please enter text", preferredStyle: .alert)
     let editAction = UIAlertAction(title: "Save", style: .default) { _ in
         guard let note = alert.textFields?.first?.text, !note.isEmpty else { return }
-        self.edit(note: noteForCell, with: note)
+        StorageManager.shared.edit(noteForCell, newName: note)
+        self.tableView.reloadData()
     }
     let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
     alert.addAction(editAction)
     alert.addAction(cancelAction)
     alert.addTextField { textField in
         textField.placeholder = "Edit Note"
+        textField.text = noteForCell.text
     }
     present(alert, animated: true)
         
     }
     
-    func reloadData() {
-        fetchData()
+    func filterList(_ notelistForSort: [Note]) {
+        
+        let newNoteList = notelistForSort.sorted(by: { $0.text?.lowercased() ?? "a" < $1.text?.lowercased() ?? "b"})
+        notesList = newNoteList
         tableView.reloadData()
     }
+    
 }
--- NotesApp/StorageManager.swift
+++ NotesApp/StorageManager.swift
@@ -23,8 +23,41 @@ class StorageManager {
 
     private init () { viewContext = persistentContainer.viewContext }
     
-
+    // MARK: - Public Methods
     
+    // fetch data
+    func fetchData(completion: (Result<[Note], Error>) -> Void) {
+        let fetchRequest = Note.fetchRequest()
+        
+        do {
+            let notes = try viewContext.fetch(fetchRequest)
+            completion(.success(notes))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    // save data
+    func save(_ noteName: String, completion: (Note) -> Void) {
+        let note = Note(context: viewContext)
+        note.text = noteName
+        completion(note)
+        saveContext()
+    }
+    
+    // edit data
+    func edit(_ note: Note, newName: String) {
+        note.text = newName
+        saveContext()
+    }
+    
+    // delete data
+    func delete(_ note: Note) {
+        viewContext.delete(note)
+        saveContext()
+    }
+
+    // MARK: - Core Data Saving support
     func saveContext() {
         let context = persistentContainer.viewContext
         if context.hasChanges {
